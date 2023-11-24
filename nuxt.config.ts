780e9d8475e1db6f228fc2e7ed3a33a93d079842
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2023-11-24 16:23:15 +0100

    ajout de scss avec les styles du projets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,31 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
+  // empecher Nuxt de préfixer les composants
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
+    },
+  ],
+
   runtimeConfig: {
     public: {
       challongeApiUrl: "",
+    },
+  },
+
+  css: ["@/scss/main.scss"],
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/scss/foundations/_variables.scss";
+                          @import "@/scss/foundations/_mixins.scss";
+          `,
+        },
+      },
     },
   },
 })
