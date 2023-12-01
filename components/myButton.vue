04e9a861b8aab214e3fbc7dbdb989479e02f6809
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2023-12-01 16:31:24 +0100

    création du bouton et ses variants

---
+++ components/myButton.vue
@@ -0,0 +1,66 @@
+<template>
+    <button class="my_button" :class="className">
+        <span class="my_button-text"><slot/></span>
+    </button>
+</template>
+
+<style lang="scss">
+.my_button{
+    flex: none;
+    display: inline-flex;
+    padding: $m-small $m-small+10;
+    min-width: 100px;
+    height: fit-content;
+    background: $color-white;
+    transform: skewX(-25deg);
+    text-align: center;
+    color: $color_black;
+    
+    &-text{
+        width: 100%;
+        transform: skewX(25deg);
+    }
+    
+    &:hover{
+        background: $color-white_darken;
+    }
+    
+    &.-big{
+        @include bouton_big;
+        padding: $m-litle $m-litle+15;
+        min-width: 150px;
+    }
+    
+    &.-blue{
+        background: $color-main;
+        color: $color_white;
+
+        &:hover{
+            background: $color-main_darken;
+        }
+    }
+
+    &.-non_clicable{
+        background: $color-gray;
+        cursor: not-allowed;
+        
+        &:hover{
+            background: $color-gray;
+        }
+    }
+}
+</style>
+
+<script setup>
+const props = defineProps({
+  size: String,
+  color: String,
+  clicable: Boolean,
+});
+
+const className = computed(() => ({
+  " -big": props.size === "big",
+  " -blue": props.color === "blue",
+  " -non_clicable": props.clicable === "false",
+}));
+</script>
