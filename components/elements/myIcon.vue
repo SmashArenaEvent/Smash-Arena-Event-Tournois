7fce0cbb674f2a9b15d3b00f5e192914a49b85f9
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2023-12-07 10:48:05 +0100

    création des icons

---
+++ components/elements/myIcon.vue
@@ -0,0 +1,52 @@
+<template>
+    <i class="icon">
+        <a :href="url" target="_blank" rel="noopener noreferrer">
+            <component :is="getIcon"/>
+        </a>
+    </i>
+</template>
+
+<style lang="scss">
+.icon{
+    display: block;
+    width: 50px;
+    height: 50px;
+    padding: 25%;
+    border: $color-white 1px solid;
+    border-radius: 100%;
+    text-align: center;
+    fill: $color-white;
+    transition: all 0.5s;
+    cursor: pointer;
+
+    &:hover{
+        background-color: $color-main;
+    };
+}
+</style>
+
+<script setup>
+import iconInsta from '@/components/icons/iconInsta.vue'
+import iconX from '@/components/icons/iconX.vue'
+import iconFacebook from '@/components/icons/iconFacebook.vue'
+import iconTwitch from '@/components/icons/iconTwitch.vue'
+
+const props = defineProps({
+  name: String,
+  url: String,
+})
+
+const getIcon = computed(() => {
+  switch (props.name) {
+    case 'twitch':
+        return iconTwitch
+    case 'x':
+        return iconX
+    case 'facebook':
+        return iconFacebook
+    default:
+        return iconInsta
+  }
+})
+
+</script>
---
+++ components/icons/iconX.vue
@@ -0,0 +1,6 @@
+<template>
+    <svg width="100%" viewBox="0 0 24 24" fill="inherit" xmlns="http://www.w3.org/2000/svg">
+        <path d="M24.0002 0L3.63456 24H1.2002L21.5665 0H24.0002Z"/>
+        <path d="M6.5209 2L20.2107 22H17.4791L3.78933 2H6.5209ZM7.57202 0H0L16.428 24H24L7.57202 0Z"/>
+    </svg>
+</template>
