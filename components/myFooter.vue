4ce140a504be1a9a45c6d59d81fde9a88fe86575
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2024-01-15 15:02:39 +0100

    utilisation de flex-end pour supprimer les warning

--- components/myFooter.vue
+++ components/myFooter.vue
@@ -77,7 +77,7 @@
         ul{
             width: 100%;
             display: flex;
-            justify-content: end;
+            justify-content: flex-end;
             gap: 10px;
 
             li{
