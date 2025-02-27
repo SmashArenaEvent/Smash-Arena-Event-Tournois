da0606e26b9237ee608797c36675f0a4bd5ca1fe
Barder_Lucas <bardey.estelle1@gmail.com>
2025-02-27 12:08:22 +0100

    build fonctionnel

--- pages/edition/index.vue
+++ pages/edition/index.vue
@@ -1,9 +1,15 @@
 <script lang="ts">
+import { defineComponent } from 'vue';
 import { RouterLink } from 'vue-router';
 
-
+export default defineComponent({
+  name: 'EditionIndex',
+});
 </script>
+
 <template>
-    <RouterLink to="./edition/edition2024">2024</RouterLink>
-    <RouterLink to="./edition/edition2025">2025</RouterLink>
+  <div>
+    <RouterLink to="./edition2024">2024</RouterLink>
+    <RouterLink to="./edition2025">2025</RouterLink>
+  </div>
 </template>
