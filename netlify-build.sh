f2516fce005989bee640f13c23dda6d0d0479dd0
Barder_Lucas <bardey.estelle1@gmail.com>
2025-03-04 11:45:32 +0100

    netlify parametrage

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+
+# Configurer Git avec le token d'authentification
 git config --global url."https://${GITHUB_TOKEN}@github.com".insteadOf "https://github.com"
+
+# Mettre à jour les sous-modules
 git submodule update --init --recursive
+
+# Autres étapes nécessaires pour le build, comme npm install
+npm install
