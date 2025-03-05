64ea29ab39d4c0162a461b4e9bacee6f60b29685
Barder_Lucas <bardey.estelle1@gmail.com>
2025-03-05 10:29:04 +0100

    mise en fonction de netlify

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Arrêt en cas d'erreur
+set -e
+
 # Configurer Git avec le token d'authentification
 git config --global url."https://${Smash_Arena_Event_Site_globale}@github.com".insteadOf "https://github.com"
 
@@ -8,3 +11,9 @@ git submodule update --init --recursive
 
 # Autres étapes nécessaires pour le build, comme npm install
 npm install
+
+# Génération du build (ajustez selon votre framework)
+npm run build
+
+# Vérification du contenu du dossier de build
+ls -la dist
