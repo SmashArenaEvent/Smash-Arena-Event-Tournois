8ad596b24cd1d988d00bda9d74fed79a1d20cc85
Barder_Lucas <bardey.estelle1@gmail.com>
2025-03-07 11:36:49 +0100

    test connection distant

--- utils/axios.js
+++ utils/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const CHALLONGE_API = axios.create({
-  baseURL: "https://smash-arena-event-tournois.onrender.com/api/tournaments",
+  baseURL: import.meta.env.VITE_API_URL
 });
